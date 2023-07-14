2b8f4c309b844f6217a138c561e9cd70fb8572e4
Jeffuz <jeffzhang0049@gmail.com>
2023-07-14 13:50:41 -0700

    previous page button

--- fitflow/src/Pages/Questionnaire.js
+++ fitflow/src/Pages/Questionnaire.js
@@ -1,7 +1,19 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Questionnaire() {
+    const goBack = () => {
+        window.history.back();
+    };
+
     return (
         <>
-            <h2>Test</h2>
+            <button onClick={goBack}>&#8592;</button>
+            <p>Do you have an account?</p>
+            <Link to="/login">
+                <button>Yes, I Already Have an Account</button>
+            </Link>
+            <button>Not Yet</button>
         </>
-    )
+    );
 }
