3effc97fbbc03549d39f1053600e6fa1ac8a610c
Jeffuz <jeffzhang0049@gmail.com>
2023-07-14 13:00:56 -0700

    Home Page

--- fitflow/src/App.js
+++ fitflow/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Login from './Pages/Login'
 import NoPage from './Pages/NoPage'
+import Questionnarie from './Pages/Questionnaire'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/questionnarie" element={<Questionnarie />} />
           <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
