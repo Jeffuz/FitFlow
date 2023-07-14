ad13f3e8f465c1ec02f493c3d4fbc47d4f35ef2b
Simon Huang <huisimonliu@gmail.com>
2023-07-14 13:04:38 -0700

    Fixes Jeff problem

--- fitflow/src/Pages/Home.js
+++ fitflow/src/Pages/Home.js
@@ -7,9 +7,9 @@ export default function Home() {
             <h1>FitFlow</h1>
             <h3>Unlock your potential, embrace your strength, and flow towards a healthier you with FitFlow</h3>
             <h4>
-                By tapping GET STARTED, you agree to our 
-                <a href="/">Terms of Use</a> and
-                <a href="/">Privacy Policy</a>.
+                By tapping GET STARTED, you agree to our
+                <a href="/"> Terms of Use </a>and
+                <a href="/"> Privacy Policy </a>.
                 Please review them before continuing.
             </h4>
             <Link to="/question">
