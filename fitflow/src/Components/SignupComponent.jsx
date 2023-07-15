9833327e043ed1f8b3f5a13cb95165fe4543fab5
Simon Huang <huisimonliu@gmail.com>
2023-07-14 21:39:30 -0700

    User registration now checks for valid email, password qualities

--- fitflow/src/Components/SignupComponent.jsx
+++ fitflow/src/Components/SignupComponent.jsx
@@ -1,10 +1,25 @@
 import React, {useState} from "react";
 
+const PASSWORDLENGTH = '8';
+const NUMBEROFCAPITALLETTERS = '1';
 
 export default function SignupComponent() {
 
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
+  const [isMatchingPassword, setIsMatchingPassword] = useState(false);
+
+  const checkMatchingPassword = (event) => {
+
+    let inputPassword = event.target.value;
+
+    if (password !== inputPassword) {
+      setIsMatchingPassword(false);
+      return; 
+    }
+    
+    setIsMatchingPassword(true);
+  }
 
   async function signupUser(credentials) {
 
@@ -17,6 +32,29 @@ export default function SignupComponent() {
   }
   const handleSignup = async(event) => {
     event.preventDefault();
+
+    const emailCheck = /[\w]+@[A-Za-z]+\.(.){3,}/g
+    const passLengthCheck = /(.+){6,}/g
+    const passSpecialCharacterCheck = /[-’/`~!#*$@_%+=.,^&(){}[\]|;:”<>?\\]+/g
+    const passUpperCaseCheck = /[A-Z]+/g
+
+    let isValidEmail = emailCheck.test(email);
+    let isPLength = passLengthCheck.test(password);
+    let isPSpecial = passSpecialCharacterCheck.test(password);
+    let isPUpper = passUpperCaseCheck.test(password);
+
+    if(!isValidEmail){
+      //setErrorMessage("Email is Invalid");
+      console.log("Email Invladi");
+      return;
+    }
+
+    if(!isPLength || !isPSpecial || !isPUpper || !isMatchingPassword) {
+      console.log("Password Invalid");
+      return;      
+    }
+
+
 
     let result = await signupUser({email, password});
     let convert = await result.json();
@@ -35,6 +73,10 @@ export default function SignupComponent() {
         <label>Password: </label> <br/>
 
         <input type="password" onChange={e => setPassword(e.target.value)}/> <br/>
+
+        <label>Confirm Password: </label> <br/>
+
+        <input type="password" onChange={checkMatchingPassword}/> <br/>
 
         <button type="submit">Submit</button>
       </form>
