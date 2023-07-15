ffd055dcaa4909fac2cc2312e25fddcb79fff3db
Jeffuz <jeffzhang0049@gmail.com>
2023-07-15 00:16:03 -0700

    Update HowMuchSleep.jsx

--- fitflow/src/Components/HowMuchSleep.jsx
+++ fitflow/src/Components/HowMuchSleep.jsx
@@ -1,14 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function HowMuchSleep({ onContinue }) {
+    const [selectedSleep, setSelectedSleep] = useState(null);
+
+    const handleOptionClick = (event, Sleep) => {
+        setSelectedSleep(Sleep);
+    };
 
     const handleContinue = () => {
-        onContinue();
+        if (selectedSleep) {
+            onContinue();
+        } else {
+            alert('Please select an option');
+        }
     };
 
     return (
         <>
+            <h2>How much sleep do you get?</h2>
+            <div onClick={(event) => handleOptionClick(event, 'less_than_5_hours')}>
+                <label>
+                    <strong>Less Than 5 Hours</strong> 
+                </label>
+                <input
+                    type="radio"
+                    checked={selectedSleep === 'less_than_5_hours'}
+                    onChange={() => { }}
+                />
+            </div>
+            <div onClick={(event) => handleOptionClick(event, '5-6_hours')}>
+                <label>
+                    <strong>5-6 Hours</strong> 
+                </label>
+                <input
+                    type="radio"
+                    checked={selectedSleep === '5-6_hours'}
+                    onChange={() => { }}
+                />
+            </div>
+            <div onClick={(event) => handleOptionClick(event, '7-8_hours')}>
+                <label>
+                    <strong>7-8 hours</strong> 
+                </label>
+                <input
+                    type="radio"
+                    checked={selectedSleep === '7-8_hours'}
+                    onChange={() => { }}
+                />
+            </div>
+            <div onClick={(event) => handleOptionClick(event, 'more_than_8_hours')}>
+                <label>
+                    <strong>More Than 8 Hours</strong> 
+                </label>
+                <input
+                    type="radio"
+                    checked={selectedSleep === 'more_than_8_hours'}
+                    onChange={() => { }}
+                />
+            </div>
             <button onClick={handleContinue}>Next</button>
         </>
     );
 }
+
