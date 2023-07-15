4e5a62c9be1149680c4847c07da774608e4e5c2b
Jeffuz <jeffzhang0049@gmail.com>
2023-07-15 00:49:13 -0700

    Update WhatWeight.jsx

--- fitflow/src/Components/WhatWeight.jsx
+++ fitflow/src/Components/WhatWeight.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default function WhatWeight({ onContinue }) {
+export default function WhatHeight({ onContinue }) {
+    const [weight, setWeight] = useState('');
+    const [measurementUnit, setMeasurementUnit] = useState('kg');
+
+    const handleInputChange = (event) => {
+        setWeight(event.target.value);
+    };
+
+    const handleUnitChange = (event) => {
+        setMeasurementUnit(event.target.value);
+    };
 
     const handleContinue = () => {
-        onContinue();
+
+        onContinue(weight, measurementUnit);
     };
 
     return (
         <>
+            <h2>Enter Your Weight</h2>
+            <div>
+                <label>
+                    <input
+                        type="number"
+                        value={weight}
+                        onChange={handleInputChange}
+                        placeholder={`Enter your weight in ${measurementUnit}`}
+                    />
+                    {measurementUnit}
+                </label>
+            </div>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        value="kg"
+                        checked={measurementUnit === 'kg'}
+                        onChange={handleUnitChange}
+                    />
+                    kg
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        value="lb"
+                        checked={measurementUnit === 'lb'}
+                        onChange={handleUnitChange}
+                    />
+                    lb
+                </label>
+            </div>
             <button onClick={handleContinue}>Next</button>
         </>
     );
