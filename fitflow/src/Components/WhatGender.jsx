aeeb89eb470ab06d12a3e07e4ceee1e66d5c30ac
Jeffuz <jeffzhang0049@gmail.com>
2023-07-14 20:50:22 -0700

    Update WhatGender.jsx

--- fitflow/src/Components/WhatGender.jsx
+++ fitflow/src/Components/WhatGender.jsx
@@ -1,14 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function WhatGender({ onContinue }) {
+    const [selectedGoal, setSelectedGoal] = useState(null);
+
+    const handleOptionClick = (event, goal) => {
+        setSelectedGoal(goal);
+    };
 
     const handleContinue = () => {
-        onContinue();
+        if (selectedGoal) {
+            onContinue();
+        } else {
+            alert('Please select a main goal');
+        }
     };
 
     return (
         <>
+            <h2>What's your gender?</h2>
+            <div onClick={(event) => handleOptionClick(event, 'male')}>
+                <label>
+                    <strong>Male</strong> 
+                </label>
+                <input
+                    type="radio"
+                    value="male"
+                    checked={selectedGoal === 'male'}
+                    onChange={() => { }}
+                />
+            </div>
+            <div onClick={(event) => handleOptionClick(event, 'female')}>
+                <label>
+                    <strong>Female</strong> 
+                </label>
+                <input
+                    type="radio"
+                    value="female"
+                    checked={selectedGoal === 'female'}
+                    onChange={() => { }}
+                />
+            </div>
+            <div onClick={(event) => handleOptionClick(event, 'non_binary')}>
+                <label>
+                    <strong>Non-Binary</strong> 
+                </label>
+                <input
+                    type="radio"
+                    value="non_binary"
+                    checked={selectedGoal === 'non_binary'}
+                    onChange={() => { }}
+                />
+            </div>
             <button onClick={handleContinue}>Next</button>
         </>
     );
 }
+
