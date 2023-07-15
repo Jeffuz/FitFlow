9799d06bc80d9e645ebec841a22589449ae355f7
Jeffuz <jeffzhang0049@gmail.com>
2023-07-15 00:16:05 -0700

    Update SendentaryLifestyle.jsx

--- fitflow/src/Components/SendentaryLifestyle.jsx
+++ fitflow/src/Components/SendentaryLifestyle.jsx
@@ -1,13 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SendentaryLifestyle({ onContinue }) {
+    const [selectedStyle, setSelectedStyle] = useState(null);
+
+    const handleOptionClick = (event, Style) => {
+        setSelectedStyle(Style);
+    };
 
     const handleContinue = () => {
-        onContinue();
+        if (selectedStyle) {
+            onContinue();
+        } else {
+            alert('Please select an option');
+        }
     };
 
     return (
         <>
+            <h2>Do you have a sedentary lifestyle?</h2>
+            <div onClick={(event) => handleOptionClick(event, 'yes')}>
+                <label>
+                    <strong>Yes</strong> 
+                </label>
+                <input
+                    type="radio"
+                    checked={selectedStyle === 'yes'}
+                    onChange={() => { }}
+                />
+            </div>
+            <div onClick={(event) => handleOptionClick(event, 'no')}>
+                <label>
+                    <strong>No</strong> 
+                </label>
+                <input
+                    type="radio"
+                    checked={selectedStyle === 'no'}
+                    onChange={() => { }}
+                />
+            </div>
             <button onClick={handleContinue}>Next</button>
         </>
     );
