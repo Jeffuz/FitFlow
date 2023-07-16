ec178112ccd0f52dfec6eabb7c5c238d1843cd00
Jeffuz <jeffzhang0049@gmail.com>
2023-07-15 23:39:11 -0700

    Update Login.js

--- fitflow/src/Pages/Login.js
+++ fitflow/src/Pages/Login.js
@@ -4,12 +4,13 @@ import LoginComponent from '../Components/UserAuthentication/LoginComponent';
 
 export default function Login() {
     return (
-        <div>
-            <Link to="/questionnaire">
-                <button>&#8592;</button>
+        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+            <Link to="/" className="absolute top-4 left-4">
+                <button className="bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded">
+                    &#8592; Back
+                </button>
             </Link>
-            <LoginComponent/>         
+            <LoginComponent />
         </div>
-
-    )
+    );
 }
